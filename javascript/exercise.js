fe81f92d3bc3462b080d55312a56c434927eb726
Xin Tao <xtao17@stlawu.edu>
2020-01-29 14:17:38 -0500

    exercise

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -1,5 +1,5 @@
 let sum=0;
-for (int i=1;i<=10;i++){
+for (let i=1;i<=10;i++){
   sum=sum+i;
 }
 console.log(sum);
@@ -13,21 +13,24 @@ while (b<1000){
 }
 console.log(b);
 for(let i=1;i<=100;i++){
-  if(i%3===0||i%5===0){
+  if((i%3===0||i%5===0)&&!(i%3===0&&i%5===0)){
     console.log(i);
   }
 }
-divide=true
+
 for(let i=1;;i++){
+  let divide=true;
   for(let k=1;k<=10;k++){
     if(i%k===0){
-      divide=true
+      divide=true;
     }
     else{
-      divide=false
+      divide=false;
+      break;
     }
   }
   if(divide===true){
     console.log(i);
+    break;
   }
 }
