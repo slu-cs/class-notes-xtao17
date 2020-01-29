4e465c7828ab1f28488dc56b83f4dc4e8570935c
Xin Tao <xtao17@stlawu.edu>
2020-01-29 13:44:26 -0500

    js

---
+++ javascript/expressions.js
@@ -0,0 +1,50 @@
+//logic
+!false
+true && false
+true || false
+
+//Numbers (all one type)
+3.14
+
+//Equality
+3===3.0
+3!==3.14
+
+//Ordering
+3 > 2
+3 >=2
+2<3
+2<= 3
+
+//Strings
+'hello'
+
+
+//Concatenation
+'a' + 'b'==='ab'
+
+
+// Arithmetic
+3+2===5
+3-2===1
+3*2===6
+3/2===1.5
+3%2===1
+3**2===9
+
+//Type coercion
+'3'+ 2=== '32'
+'3'+ true=== '3true'
+'3'- 2==1
+3 +true===4 //true is 1, false is 0
+
+//Loose Equality
+'3' ==3
+true ==1
+false==0
+false==''
+''==0
+
+//Template Strings
+'3+2 is ' + (3+2)
+`3+2 is ${3+2`
---
+++ javascript/statements.js
@@ -0,0 +1,25 @@
+//Statement syntax in JavaScript
+
+//constants
+const pi=3.14;
+
+//Printing
+console.log(pi);
+
+
+//for loop
+for(let x=0;x<3;x++){
+  console.log(x);
+}
+let x=0;
+while (x<3){
+  console.log(x);
+  x+=1;
+}
+//Conditional
+if (x===3){
+  console.log('x is now 3');
+}
+else{
+  console.log(`x is now ${x}`)
+}
