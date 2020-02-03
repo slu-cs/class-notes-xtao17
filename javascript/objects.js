d23fe2061407e1555f27b2be1941656f06436d67
Xin Tao <xtao17@stlawu.edu>
2020-02-03 13:07:17 -0500

    Javascript literals

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,4 +1,19 @@
 //Object and array syntax
 //Oject literal
-const point={1,2};
+const point={x:1,y:2};
 console.log(point);
+//Accessing properties
+console.log(point.x);
+//Changing properties
+point.x=3;
+//Nested object literal
+cost circle={
+  center: point,
+  result:4
+};
+//Accessing subproperties
+console.log(circle.center.x);
+//indefined subproperties
+console.log(circle.color);
+circle.color='blue';
+console.log(circle);
