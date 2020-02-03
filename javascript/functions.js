dac78ad41642e65befb20305a1c5c0cee5d5ae0c
Xin Tao <xtao17@stlawu.edu>
2020-02-03 13:34:02 -0500

    Javascript functions

--- javascript/functions.js
+++ javascript/functions.js
@@ -17,3 +17,38 @@ const result=print(7,8);
 console.log(result);
 print(9);
 print();
+
+//Object literal with a method
+const rectangle={
+  width:10,
+  height:20,
+  area:function(){
+    return this.width*this.height;
+  }
+};
+
+
+//Method call
+console.log(rectangle.area());
+
+
+//Constructor definition
+const Rectangle=function(width, height){
+  this.width=width;
+  this.height=height;
+};
+
+
+//Shared method
+Rectangle.prototype.area=function(){
+  return this.width *this.height;
+}
+
+//Constructing objects
+
+const small=new Rectangle(1,2);
+const large=new Rectangle(10,20);
+
+//Method calls
+console.log(small.area());
+console.log(large.area());
