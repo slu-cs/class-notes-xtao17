808df4bd22ce233fd8d795de02d437e75fd338b9
Xin Tao <xtao17@stlawu.edu>
2020-02-03 14:22:31 -0500

    javascript quiz practice

--- javascript/quiz2practice.js
+++ javascript/quiz2practice.js
@@ -8,5 +8,25 @@ for(let i=0;i<courses.length;i++){
   if(courses[i].number>n){
     n=courses[i].number;
   }
-}
+};
 console.log(n);
+
+// Define a constructor function for Book objects. Each book should have a title and an array of authors.
+//Define a shared method for books that lets you check
+//whether a given author was one of the authors in this book.
+const Book=function(title,author){
+  this.title=title;
+  this.author=author;
+};
+
+Book.prototype.checkauthor=function(name){
+  for(let i=0;i<Book.author.length;i++){
+    if(Book.author[i]===name){
+      console.log(True);
+    }
+  }
+  console.log(False);
+};
+
+const abook=new Book("haha","xin");
+abook.checkauthor("xin");
