b16ea5fc817462db90e665f2e4eb0baef12eb629
Xin Tao <xtao17@stlawu.edu>
2020-02-05 14:06:44 -0500

    console

--- javascript/input.js
+++ javascript/input.js
@@ -1,33 +1,29 @@
-//Syntax for console and file input
+// Syntax for console and file input
 
-//synchronous calls:wait for a result(stuck until it arrives)
-//asynchronous calls:do not wait for the result, handle the result when it arrives
-//web involves lots of asynchronous because of many users so should not stuck
-const readline=require('readline');
-//Console configuration
-//short for filesystem
-const fs=require('fs');
-const user= readline.createInterface({
+const readline = require('readline');
+const fs = require('fs');
+
+// Console configuration
+const user = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Console input
+user.question('Filename: ', function(filename) {
 
-
-//console input
-user.question('Filename:',function(filename){
-  //file configuration
-  const file=readline.createInterface({
-    input:fs.createReadStream(filename)
+  // File configuration
+  const file = readline.createInterface({
+    input: fs.createReadStream(filename)
   });
 
-//asynchronous line-by-line input
-file.on('line',function(line){
-  console.log(line);
+  // Asynchronous line-by-line input
+  file.on('line', function(line) {
+    console.log(line);
+  });
+
+  // End the program when the file closes
+  file.on('close', function() {
+    process.exit(0);
+  });
 });
-//End the program when the file closes
-file.on('close',function(){
-  process.exit(0);
-});
-//This part will not happen
-console.log('here');
