02b9e3039303691533d5b86835c6cb154192b5dc
Xin Tao <xtao17@stlawu.edu>
2020-02-05 13:16:34 -0500

    quiz

--- javascript/practice.js
+++ javascript/practice.js
@@ -9,6 +9,16 @@ Roaster.prototype.addname=function(aname){
   }
   return false;
 }
-const aroaster=new Roaster(15);
+const aroaster=new Roaster(1);
 aroaster.addname("Xin");
 console.log(aroaster.names);
+console.log(aroaster.maxstud);
+console.log(aroaster.addname("Tao");
+console.log(aroaster.names);
+
+
+const labs=[{location:"Bewkes 105", seats:10},{location:"Bewkes 107", seats:1}];
+for(lab of labs){
+  labs.seats=labs.seats*2;
+}
+console.log(labs[0].seats);
