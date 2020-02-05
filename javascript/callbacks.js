38713bfc34e4990ec75ee20efeb0510d5a65d63d
Xin Tao <xtao17@stlawu.edu>
2020-02-05 13:32:44 -0500

    arrow function

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -17,3 +17,10 @@ const numbers=[1,2,3,4,5];
 //doing the transformation
 console.log(map(numbers,square));
 console.log(numbers);
+
+//Define other transforms with arrow function
+console.log(map(numbers, x=>x**2));
+console.log(map(numbers,x=>x+1));
+//usingf the array map method
+console.log(numbers.map(x=>x**2));
+console.log(numbers.map(x=>x+1));
