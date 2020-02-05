d6321697fdd234da8a4e2bac912847b6a2a5314a
Xin Tao <xtao17@stlawu.edu>
2020-02-05 13:47:01 -0500

    console input

--- javascript/input.js
+++ javascript/input.js
@@ -3,9 +3,9 @@
 //synchronous calls:wait for a result(stuck until it arrives)
 //asynchronous calls:do not wait for the result, handle the result when it arrives
 //web involves lots of asynchronous because of many users so should not stuck
-const realine=require('readline');
+const readline=require('readline');
 //Console configuration
-const user=readline.createInterface({
+const user= readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
