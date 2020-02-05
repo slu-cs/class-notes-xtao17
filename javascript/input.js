2559efc44bdd2c064b681382886f8841a89c110f
Xin Tao <xtao17@stlawu.edu>
2020-02-05 14:17:34 -0500

    practice

--- javascript/input.js
+++ javascript/input.js
@@ -27,3 +27,17 @@ user.question('Filename: ', function(filename) {
     process.exit(0);
   });
 });
+
+const numbers = [1, 2, 3, 4, 5];
+const odds = numbers.filter(x => x%2 > 0);
+console.log(odds)
+const filter=function(numbers,f){
+  const copy=[];
+  for(const element of array){
+    copy.push(f(element));
+  }
+  return copy;
+}
+const numbers = [1, 2, 3, 4, 5];
+const odds1 = filter(numbers, x => x%2 > 0);
+console.log(odds1)
