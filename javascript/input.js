1e0c05fd15de95d40bbe73ff107a77cc023e01dd
Xin Tao <xtao17@stlawu.edu>
2020-02-05 13:59:32 -0500

    Javascript file input

--- javascript/input.js
+++ javascript/input.js
@@ -5,16 +5,30 @@
 //web involves lots of asynchronous because of many users so should not stuck
 const readline=require('readline');
 //Console configuration
+//short for filesystem
+const fs=require('fs');
 const user= readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 
+
 //console input
 user.question('Filename:',function(filename){
-  console.log(filename);
+  //file configuration
+  const file=readline.createInterface({
+    input:fs.createReadStream(filename);
+  })
 })
 
+//asynchronous line-by-line input
+file.on('line',function(line){
+  console.log(line);
+});
+//End the program when the file closes
+file.on('close',function(){
+  process.exit(0);
+});
 //This part will not happen
 console.log('here');
