30681a5c80581e4a0dc70364549d6109f5665c72
Xin Tao <xtao17@stlawu.edu>
2020-03-04 14:17:21 -0500

    3.4

--- express/bakery/app.js
+++ express/bakery/app.js
@@ -1,4 +1,3 @@
-
 // Bakery web server
 const express = require('express');
 
@@ -7,26 +6,26 @@ const app = express();
 
 // Ignore icon requests
 app.get('/favicon.ico', function(request, response) {
-response.status(204).end();
+  response.status(204).end();
 });
 
 // Log requests to the console
 app.use(function(request, response, next) {
-console.log('---------------------', new Date().toLocaleTimeString());
-console.log(request.method, request.url);
-console.log('Body =', request.body);
-next(); // Keep handling this request
+  console.log('---------------------', new Date().toLocaleTimeString());
+  console.log(request.method, request.url);
+  console.log('Body =', request.body);
+  next(); // Keep handling this request
 });
 
 // cs-linuxlab-##.stlawu.edu:3000/
 app.get('/', function(request, response) {
-response.send(`
-  <h1>Bakery</h1>
-  <ul>
-    <li><a href="/cakes">Cakes</a></li>
-    <li><a href="/pies">Pies</a></li>
-  </ul>
-`);
+  response.send(`
+    <h1>Bakery</h1>
+    <ul>
+      <li><a href="/cakes">Cakes</a></li>
+      <li><a href="/pies">Pies</a></li>
+    </ul>
+  `);
 });
 
 // Routing
@@ -35,14 +34,14 @@ app.use('/pies', require('./pies.js'));
 
 // Handle undefined routes
 app.use(function(request, response, next) {
-console.log('Replied with 404');
-response.status(404).end();
+  console.log('Replied with 404');
+  response.status(404).end();
 });
 
 // Handle other errors
 app.use(function(error, request, response, next) {
-console.error(error.stack);
-response.status(500).send(error.message);
+  console.error(error.stack);
+  response.status(500).send(error.message);
 });
 
 // Start the server
