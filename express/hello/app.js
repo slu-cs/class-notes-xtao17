9f8a9a7a1b67e290dfc86e9e9e04f4a129ed05ec
Xin Tao <xtao17@stlawu.edu>
2020-03-04 13:36:33 -0500

    express

--- express/hello/app.js
+++ express/hello/app.js
@@ -1,18 +1,34 @@
-//Hello World web server
+// Hello world web server
+const express = require('express');
 
-const express=require('express');
+// Create the server
+const app = express();
 
-//crete the server
-const app=express();
+// cs-linuxlab-09.stlawu.edu:3000/
+app.get('/', function(request, response) {
+  response.send('Hello Lisa');
+});
 
-//Respond to a single request
-app.get('/',function(request,response){
-  response.send('Hello Xin');
-})
+// cs-linuxlab-09.stlawu.edu:3000/foo
+app.get('/foo', function(request, response) {
+  response.send('Hello foo');
+});
 
+// cs-linuxlab-09.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response) {
+  response.send('Hello bar');
+});
 
+// cs-linuxlab-09.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+});
 
-//Start the server
-//adress for the server
+// cs-linuxlab-09.stlawu.edu:3000/zap?x=&y=
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
+});
+
+// Start the server
 app.listen(3000);
-console.log('Server is ready');
+console.log('Server is ready.');
